fd2e42b3e51d49a248604ec80ac515059393ebb0
Cookie419 <eraldachungcolon419@gmail.com>
2020-06-10 21:05:34 -0400

    "Added Authentication"
    after:  git add dot ,   To make sure we added all new files.     Always do this 1st.

---
+++ auth.js
@@ -0,0 +1,9 @@
+## feature/implementing-auth
+
+function authenticate(login, password) {
+    if (login === 'login' && Password ==='password') {
+        return 'You are logged in' ;
+    }  else  {
+       return 'Login is not correct ' ;
+    }
+}
