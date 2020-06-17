eddba618fbcc392a6599a2f98b87ccc11108bc06
Cookie419 <eraldachungcolon419@gmail.com>
2020-06-16 22:13:50 -0400

    Added exclamation to end of created.
    Small change.

--- register.js
+++ register.js
@@ -1,5 +1,5 @@
 function register(login,password) {
-    return'Account:' +' '+ login + ' ' + password + '' + 'was created';
+    return'Account:' +' '+ login + ' ' + password + '' + 'was created!';
 }
 Added
 done
